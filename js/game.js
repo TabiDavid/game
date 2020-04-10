7a5153df49ac08fd99a2fb1c9c61c92e858641ed
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-04-10 18:00:12 -0500

    commit

--- js/game.js
+++ js/game.js
@@ -1,9 +1,36 @@
-function startGame {
+function startGame (){
+gameLoop();
+}
 
-};
+var loops = 0;
+var peopleVisible = false;
 
-var count = 0;
-function undateCount{
-    count = count + 1;
-   document.getElementById("number").innerHTML = count; 
+function gameLoop(){
+    peopleVisible = !peopleVisible;
+    flashCharacter();
+    loops++;
+    if(loops <　12) {
+        setTimeout(gameLoop, 3000);
+    }
+    else {
+        alert("Game Over!") ;
+    }   
+}
+
+//var count = 0;
+
+///function updateCount(){
+   // count = count + 1;
+   //document.getElementById("number").innerHTML = count;
+   //setTimeout(updateCount, 1000); 
+//}
+
+
+function flashCharacter (){
+    var board = document.getElementById("board");
+    var classToset = peopleVisible ? "character visible" : "character hidden";
+  
+    for (var index = 0; index < 6; index++){
+        board.children[index].className = classToset;
+    }
 }
